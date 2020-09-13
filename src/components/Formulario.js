7e3c915961d3049ba73dcd9679324387ed33391a
Frank Bonilla <frankdeivis_17@hotmail.com>
2020-09-13 17:22:25 +0200

    first commit

---
+++ src/components/Formulario.js
@@ -0,0 +1,72 @@
+import React, { useContext, useState} from 'react';
+import { CategoriasContext } from '../context/CategoriaContext';
+import { RecetasContext } from '../context/RecetasContext';
+
+const Formulario = () => {
+
+    const [ busqueda, guardarBusqueda ] = useState({
+        nombre: '',
+        categoria: ''
+    });
+
+    const { categorias } = useContext(CategoriasContext);
+    const { buscarRecetas, guardarConsultar } = useContext(RecetasContext)
+
+    //funcion para leer los contenidos
+    const obtenerDatosRecetas = e => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        })
+    }
+    
+
+    return(
+        <form
+            className="col-12"
+            onSubmit={ e => {
+                e.preventDefault();
+                buscarRecetas(busqueda);
+                guardarConsultar(true);
+            }}
+                >
+            <fieldset className="text-center">
+                <legend>Busca Bebidas por Categoría o Ingrediente</legend>
+            </fieldset>
+            <div className="row mt-3">
+                <div className="col-md-4 mt-3">
+                    <input 
+                        type="text"
+                        name="nombre"
+                        className="form-control"
+                        placeholder="Buscar por ingrediente"
+                        onChange={obtenerDatosRecetas}
+                    />  
+                </div>
+                <div className="col-md-4 mt-3">
+                    <select
+                           className="form-control"
+                           name="categoria" 
+                           onChange={obtenerDatosRecetas}
+                            >
+                        <option value="">-- Selecciona Categoría --</option> 
+                        { categorias.map(categoria => (
+                            <option 
+                                key={categoria.strCategory}
+                                value={categoria.strCategory}
+                            >{categoria.strCategory}</option>
+                        ))}       
+                    </select>
+                </div>
+                <div className="col-md-4 mt-3">
+                    <input 
+                        type="submit"
+                        className="btn btn-block btn-primary"
+                        value="Buscar Bebidas"
+                        />
+                </div>
+            </div>
+        </form>
+    )
+}
+export default Formulario;
